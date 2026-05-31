20bb2308eca518edb3255005ae0aa206c49af8fe
fanzhuoAWA <fan_zhuo@outlook.com>
2026-05-31 18:22:43 +0800

    fix:修复域名错误

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://featured.fanzhuo.xyz/",
+	site: "https://shg.fanzhuo.xyz/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
