42cefb7183acb3952ba3319710fa614bc0043f10
fanzhuoAWA <fan_zhuo@outlook.com>
2026-05-31 18:14:33 +0800

    feat:新增文章

--- src/config.ts
+++ src/config.ts
@@ -17,8 +17,8 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: true,
-		src: "assets/images/background.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+		src: "assets/images/background.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		position: "bottom", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: false, // Display the credit text of the banner image
 			text: "", // Credit text to be displayed
--- src/content/posts/example.md
+++ src/content/posts/example.md
@@ -9,3 +9,13 @@ draft: true
 ## 测试
 
 ### 测试
+
+首先，让 Git 知道你是谁：git config --global user.name "你的GitHub用户名" 和 git config --global user.email "你的GitHub邮箱@example.com"
+
+然后，将远程仓库地址改成 SSH 形式（如果你本来就是通过 SSH 克隆的，就不需要再修改）：git remote set-url origin git@github.com:xxx/xxx
+
+随后，提交所有变更：git add .
+
+接着，创建一次本地提交：git commit -m "项目初始化"
+
+最后，把本地更改推送到远程仓库：git push
